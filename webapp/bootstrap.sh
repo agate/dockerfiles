fcfd6ecc0281d22e91bad2fd45dd069a89c294d0
agate <agate.hao@gmail.com>
2015-09-21 13:09:06 +0000

    fixed few problems

--- webapp/bootstrap.sh
+++ webapp/bootstrap.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+HOME=/root
 GIT_BRANCH=${GIT_BRANCH:-"master"}
 
 echo "$SSH_KEY" > $HOME/.ssh/id_rsa; chmod 600 $HOME/.ssh/id_rsa
+
 cd /home/app/webapp
 git clone $GIT_REPO --branch $GIT_BRANCH --single-branch .
 bundle install --path vendor/bundle
